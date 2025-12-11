e037d70af2a96cda9307cc5f6529fa385087d941
felipenino13 <cristianfelipenino@gmail.com>
2025-12-11 13:04:28 -0500

    add logo image tecmd

--- app/tecnico-profesional-en-programacion-de-software/page.tsx
+++ app/tecnico-profesional-en-programacion-de-software/page.tsx
@@ -1,5 +1,6 @@
 // app/tecnico-profesional-en-operaciones-de-comercio-electronico/page.tsx
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Técnico Profesional en Operaciones de Comercio Electrónico",
@@ -15,8 +16,17 @@ export default function TecnicoProfesionalPage() {
         </div>
     </div>
 
-    <div className="bg-[#313549] px-2 py-20">
-        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2">
+    <div className="bg-[#212844] px-2 py-20">
+        <div className="max-w-5xl mx-auto">
+            <Image
+                src="/logos/logos-TEC-MD-yellow_pq.png"   // ruta relativa a /public
+                alt="Estudiantes del programa técnico profesional en operaciones de comercio electrónico"
+                width={160}            // ancho real de la imagen
+                height={300}            // alto real de la imagen
+                priority                // solo si es imagen principal above the fold
+            />
+        </div>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-x-20 gap-y-10">
             <div className="grid gap-y-4">
                 <div>
                     <h1 className="text-5xl text-white">Técnico Profesional en Programación de Software</h1>
@@ -30,8 +40,8 @@ export default function TecnicoProfesionalPage() {
                 <p className="text-white">
                     $2.560.000 Precio Ministerio de Educación Nacional (MEN)
                 </p>
-                <p className="text-white">
-                    Matrícula con subsidio: <span className="text-2xl">$1.417.000</span>    
+                <p className="text-[#D5A202]">
+                    Matrícula con subsidio: <span className="text-2xl text-white">$1.417.000</span>    
                 </p>
                 <div>
                     <p className="text-sm text-white">
@@ -42,7 +52,9 @@ export default function TecnicoProfesionalPage() {
                 </div>
             </div>
             <div>
-                    form
+                <div className="bg-[#293551] p-10 shadow-md rounded-lg">
+                    <p className="text-lg text-white text-center">Solicita más información</p>
+                </div>
             </div>
         </div>
     </div>
