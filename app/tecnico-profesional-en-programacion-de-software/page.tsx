e24fa960d2395724add239193bbfa52cf2b31c4b
felipenino13 <cristianfelipenino@gmail.com>
2025-12-11 16:01:37 -0500

    se agrega la seccion de la estudainte

--- app/tecnico-profesional-en-programacion-de-software/page.tsx
+++ app/tecnico-profesional-en-programacion-de-software/page.tsx
@@ -92,6 +92,76 @@ export default function TecnicoProfesionalPage() {
             </div>
         </div>
     </div>
+
+    <div className="bg-[#FFC316] px-2 py-20">
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-x-20 gap-y-10">
+            <div className="grid gap-y-2">
+                <Image
+                src="/images/TecMD-45subsidio.webp"   // ruta relativa a /public
+                alt="Estudiantes del programa técnico profesional en operaciones de comercio electrónico"
+                width={560}            // ancho real de la imagen
+                height={300}            // alto real de la imagen
+                />
+            </div>
+            <div className="grid gap-y-2 content-center">
+                <h2 className="text-xl text-black">¿Te interesa nuestro Subsidio del 45%?</h2>
+                <p className="text-black">
+                    ¡Todos nuestros programas cuentan con subsidio para que estudies 
+                    fácilmente el programa que quieres! Estamos comprometidos con la 
+                    educación superior de calidad en Colombia y creemos en que todas 
+                    las personas pueden acceder a ella.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div className="bg-[#FFFFFF] px-2 py-20">
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-x-20 gap-y-10">
+            <div className="content-center grid gap-y-2">
+                <h2 className="text-xl text-black">¿Qué lograrás con esta formación?</h2>
+                <ul className="pl-5 text-black text-sm list-image-[url(/icons/check-icon.svg)]">
+                        <li>
+                            Escribir código eficiente y estructurado para diferentes aplicaciones.
+                        </li>
+                        <li>
+                            Realizar pruebas de software y corregir errores para mejorar su rendimiento.
+                        </li>
+                        <li>
+                            Aplicar metodologías y herramientas de desarrollo bajo estándares de calidad.
+                        </li>
+                        <li>
+                            Desarrollar soluciones tecnológicas innovadoras para distintos contextos.
+                        </li>
+                        <li>
+                            Demostrar liderazgo, ética y compromiso en entornos académicos y laborales.
+                        </li>
+                </ul>
+                <p className="text-black">
+                    <b>¡No te quedes atrás en la era digital!</b>
+                </p>
+            </div>
+            <div className="relative">
+                <div>
+                    <Image
+                    src="/images/student-tecmd.jpg"   // ruta relativa a /public
+                    alt="Estudiantes del programa técnico profesional en operaciones de comercio electrónico"
+                    width={560}            // ancho real de la imagen
+                    height={300}            // alto real de la imagen
+                    />
+                </div>
+                <div className="absolute bottom-0 left-0 bg-white m-5 p-5 max-w-2xs grid gap-y-2">
+                    <p className="text-black text-sm">
+                        Conviértete en Técnico Profesional en Programación de Software 
+                        y prepárate para una carrera llena de oportunidades.
+                    </p>
+                    <p className="text-black text-sm">
+                        <b>¡Inscríbete ahora y toma una decisión inTECligente para 
+                        asegurar tu futuro!</b>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
     </>
   );
 }
