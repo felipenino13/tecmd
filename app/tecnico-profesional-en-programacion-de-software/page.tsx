edff277c5ea984bd5996ebeabb53a8d48f066e91
felipenino13 <cristianfelipenino@gmail.com>
2025-12-11 10:36:58 -0500

    add class color text

--- app/tecnico-profesional-en-programacion-de-software/page.tsx
+++ app/tecnico-profesional-en-programacion-de-software/page.tsx
@@ -20,21 +20,21 @@ export default function TecnicoProfesionalPage() {
             <div className="grid gap-y-4">
                 <div>
                     <h1 className="text-5xl text-white">Técnico Profesional en Programación de Software</h1>
-                    <p className="text-base">Desarrolla código, resuelve problemas y lidera en la transformación social a través del software</p>
+                    <p className="text-base text-white">Desarrolla código, resuelve problemas y lidera en la transformación social a través del software</p>
                 </div>
                 <div className="flex gap-2">
-                    <p className="text-sm rounded-xl px-2 py-1 border border-white">4 Periodos académicos</p>
-                    <p className="text-sm rounded-xl px-2 py-1 border border-white">65 Créditos</p>
-                    <p className="text-sm rounded-xl px-2 py-1 border border-white">100% Virtual</p>
+                    <p className="text-sm rounded-xl px-2 py-1 border border-white text-white">4 Periodos académicos</p>
+                    <p className="text-sm rounded-xl px-2 py-1 border border-white text-white">65 Créditos</p>
+                    <p className="text-sm rounded-xl px-2 py-1 border border-white text-white">100% Virtual</p>
                 </div>
-                <p>
+                <p className="text-white">
                     $2.560.000 Precio Ministerio de Educación Nacional (MEN)
                 </p>
-                <p>
+                <p className="text-white">
                     Matrícula con subsidio: <span className="text-2xl">$1.417.000</span>    
                 </p>
                 <div>
-                    <p className="text-sm">
+                    <p className="text-sm text-white">
                         Código SNIES: 116692<br></br>
                         Resolución Registro calificado 000631 – 01/25/2024<br></br>
                         Vigencia Registro Calificado 7 años
