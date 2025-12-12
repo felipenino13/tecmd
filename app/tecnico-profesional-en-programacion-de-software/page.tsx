3d92c16acb59b7572d63865817eee4221583c304
felipenino13 <cristianfelipenino@gmail.com>
2025-12-12 08:50:55 -0500

    se agrega nuevos componentes hasta el paso a paso

--- app/tecnico-profesional-en-programacion-de-software/page.tsx
+++ app/tecnico-profesional-en-programacion-de-software/page.tsx
@@ -162,6 +162,146 @@ export default function TecnicoProfesionalPage() {
             </div>
         </div>
     </div>
+
+    <div className="bg-[#FFC316] px-2 py-20">
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-x-20 gap-y-10 bg-[#FFFFFF]">
+            <div className="bg-[url(/images/tp-operaiones-shoftware.webp)] bg-center">
+
+            </div>
+            <div className="content-center grid gap-y-2 p-10">
+                <h2 className="text-xl text-black">Podrás desempeñarte como:</h2>
+                <ul className="pl-5 text-black text-sm list-image-[url(/icons/check-icon.svg)]">
+                        <li>
+                            Desarrollador de aplicaciones móviles.
+                        </li>
+                        <li>
+                            Desarrollador web.
+                        </li>
+                        <li>
+                            Tester de software.
+                        </li>
+                        <li>
+                            Consultor de tecnología de la información.
+                        </li>
+                        <li>
+                            Consultor de tecnología de la información.
+                        </li>
+                        <li>
+                            Ingeniero de software.
+                        </li>
+                        <li>
+                            Emprendedor en el sector tecnológico.
+                        </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div className="bg-[#212844] px-2 py-20">
+        <div className="max-w-5xl mx-auto grid grid-cols-1 gap-x-20 py-4">
+            <h2 className="text-white text-xl text-center">Beneficios de estudiar con nosotros</h2>
+        </div>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-3 gap-x-20 gap-y-10">
+            <div className="bg-[#293551] p-4 grid gap-y-4 content-between">
+                <div>
+                    <h3 className="text-lg">Financiación</h3>
+                    <p>En TecMD te financiamos el 45% de tu matrícula, además con el apoyo nuestros 
+                        aliados financieros puedes comenzar tu Técnico Profesional pagando menos 
+                        de $500.000​.
+                    </p> 
+                </div>
+                <Image
+                src="/images/financiacion-icon.webp"   // ruta relativa a /public
+                alt="Estudiantes del programa técnico profesional en operaciones de comercio electrónico"
+                width={100}            // ancho real de la imagen
+                height={300}            // alto real de la imagen
+                />               
+            </div>
+            <div className="bg-[#293551] p-4 grid gap-y-4 content-between">
+                <div>
+                    <h3 className="text-lg">Modalidad virtual</h3>
+                    <p>¡Estudia a tu propio ritmo! Nuestra modalidad virtual te ofrece la 
+                        flexibilidad para estudiar mientras trabajas o para equilibrar tu 
+                        vida con tus actividades diarias.</p>
+                </div>
+                <Image
+                src="/images/virtual-icon.webp"   // ruta relativa a /public
+                alt="Estudiantes del programa técnico profesional en operaciones de comercio electrónico"
+                width={100}            // ancho real de la imagen
+                height={300}            // alto real de la imagen
+                />  
+            </div>
+            <div className="bg-[#293551] p-4 grid gap-y-4 content-between">
+                <div>
+                    <h3 className="text-lg">Gradúate en menos tiempo</h3>
+                    <p>Estudiar y graduarte como Técnico Profesional en poco tiempo te 
+                        permite ingresar al mundo laboral más rápido, impulsando tu carrera 
+                        sin perder años de preparación. Aprovecha la oportunidad de avanzar 
+                        rápidamente hacia tu futuro.</p>
+                </div>
+                <Image
+                src="/images/tiempo-icon.webp"   // ruta relativa a /public
+                alt="Estudiantes del programa técnico profesional en operaciones de comercio electrónico"
+                width={80}            // ancho real de la imagen
+                height={300}            // alto real de la imagen
+                />  
+            </div>
+        </div>
+    </div>
+
+    <div className="bg-[#F5F5F5] px-2 py-20">
+        <div className="max-w-5xl mx-auto grid grid-cols-1 gap-x-20 py-4">
+            <h2 className="text-black text-xl text-center">Conoce nuestro proceso de inscripción</h2>
+        </div>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-5 gap-x-5 gap-y-10">
+            <div className="bg-[#FFFFFF] p-4 grid gap-y-4 content-start">
+                <div className="flex aspect-[1/1] bg-[#FFCB29] max-w-[40] rounded-[100] content-center">
+                    <p className="text-white text-center w-full m-auto">1</p>
+                </div>
+                <p className="text-black">Diligencia el formulario</p>
+            </div>
+            <div className="bg-[#FFFFFF] p-4 grid gap-y-4 content-start">
+                <div className="flex aspect-[1/1] bg-[#FF2C2C] max-w-[40] rounded-[100] content-center">
+                    <p className="text-white text-center w-full m-auto">2</p>
+                </div>
+                <p className="text-black">Reúne estos documentos:</p>
+                <ul className="pl-5 text-black text-sm list-image-[url(/icons/check-icon.svg)]">
+                        <li>
+                            Fotocopia de tu documento de identidad.
+                        </li>
+                        <li>
+                            Fotografía digital en fondo blanco.
+                        </li>
+                        <li>
+                            Diploma o acta de grado de bachiller.
+                        </li>
+                        <li>
+                            Resultados de tus pruebas ICFES Saber 11°
+                        </li>
+                </ul>
+            </div>
+            <div className="bg-[#FFFFFF] p-4 grid gap-y-4 content-start">
+                <div className="flex aspect-[1/1] bg-[#C62868] max-w-[40] rounded-[100] content-center">
+                    <p className="text-white text-center w-full m-auto">3</p>
+                </div>
+                <p className="text-black">El Departamento de Admisiones revisará tu inscripción.</p>
+            </div>
+            <div className="bg-[#FFFFFF] p-4 grid gap-y-4 content-start">
+                <div className="flex aspect-[1/1] bg-[#8E258C] max-w-[40] rounded-[100] content-center">
+                    <p className="text-white text-center w-full m-auto">4</p>
+                </div>
+                <p className="text-black">Realiza el pago en línea por PSE o de forma presencial en el 
+                    Banco de Bogotá y envía el comprobante a tu asesor educativo por correo.</p>
+            </div>
+            <div className="bg-[#FFFFFF] p-4 grid gap-y-4 content-start">
+                <div className="flex aspect-[1/1] bg-[#212844] max-w-[40] rounded-[100] content-center">
+                    <p className="text-white text-center w-full m-auto">5</p>
+                </div>
+                <p className="text-black">Revisa tu correo para información sobre clases, 
+                    calendario y usuario institucional.</p>
+            </div>
+        </div>
+    </div>
     </>
   );
 }
