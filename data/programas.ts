4df315bd37b67938e1a8c0d38055ca7fff99ebca
Cris <cristianfelipenino@gmail.com>
2025-12-18 15:42:59 -0500

    salvar lo del dia

--- data/programas.ts
+++ data/programas.ts
@@ -2,11 +2,8 @@
 
 export const INFO_GENERAL = {
   precioMEN: {
-    label: "Precio MEN",
     value: 2560000,
     formatted: "$2.560.000",
-    currency: "COP",
-    note: "Precio aprobado por el Ministerio de Educación Nacional",
   },
   matriculaSubsidio: {
     value: 1417000,
