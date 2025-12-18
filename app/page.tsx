3d83a89f1ab4c28b642655fe0936d9c21ded4e4b
Cris <cristianfelipenino@gmail.com>
2025-12-18 11:05:32 -0500

    se agrega tag manager y se actualiza un poco el home

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-[#212844] p-2 z-20">
-          <div className="max-w-5xl mx-auto grid content-center">
+          <div className="max-w-5xl mx-auto">
             <Image
                 src="/logos/logos-TEC-MD-yellow_pq.png"   // ruta relativa a /public
                 alt="Estudiantes del programa técnico profesional en operaciones de comercio electrónico"
@@ -23,9 +23,13 @@ export default function Home() {
             <h1 className="text-white">Programas TECMD</h1>
           </div>
       </div>
-      <Link href="/tecnico-profesional-en-programacion-de-software">
-        Técnico Profesional en Programación de Software..
-      </Link>
+      <div className="bg-[#212844] p-2 z-20 pb-20">
+        <div className="max-w-5xl mx-auto">
+          <Link className="text-[#ffc316] underline" href="/tecnico-profesional-en-programacion-de-software">
+            Técnico Profesional en Programación de Software
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
