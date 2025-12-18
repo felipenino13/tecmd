50d36c86da260fecd7edf62aaa3451dceada95b6
Cris <cristianfelipenino@gmail.com>
2025-12-18 07:16:38 -0500

    se quita el lazyOnLoad para ver si lo reconoce RD Station

--- app/tecnico-profesional-en-programacion-de-software/page.tsx
+++ app/tecnico-profesional-en-programacion-de-software/page.tsx
@@ -592,7 +592,7 @@ export default function TecnicoProfesionalPage() {
 
     <Script
           src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/84c87fc8-5087-4014-afc7-dd897223b353-loader.js"
-          strategy="lazyOnload"
+          //strategy="lazyOnload"
         />
     </>
   );
